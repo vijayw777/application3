337626c0b62793c0e5231fa855755c64a32e3ca6
D5_63325_Vijay_Walekar <vijay.walekar777@gmail.com>
2022-06-20 18:56:26 +0530

    added isEVEN function

--- page1.js
+++ page1.js
@@ -1,1 +1,9 @@
-console.log('helo world')
+function isEven(number) {
+  if (number % 2 == 0) {
+    console.log(`${number} is EVEN`)
+  } else {
+    console.log(`${number} is NOT EVEN`)
+  }
+}
+isEven(10)
+isEven(11)
