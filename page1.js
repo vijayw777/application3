e10f2cab6a560051c8a9a2204affee74249823b1
D5_63325_Vijay_Walekar <vijay.walekar777@gmail.com>
2022-06-20 19:04:37 +0530

    added isOdd function

--- page1.js
+++ page1.js
@@ -7,3 +7,13 @@ function isEven(number) {
 }
 isEven(10)
 isEven(11)
+
+function isOdd(number) {
+  if (number % 2 != 0) {
+    console.log(`${number} is ODD`)
+  } else {
+    console.log(`${number} is NOT ODD`)
+  }
+}
+isOdd(10)
+isOdd(11)
